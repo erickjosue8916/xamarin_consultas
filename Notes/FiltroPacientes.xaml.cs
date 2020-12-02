af8e4798047bd99fca91707175abe19ec9fe48e0
jalex099 <mm18014@ues.edu.sv>
2020-12-02 12:58:57 -0600

    Filtros page

---
+++ Notes/FiltroPacientes.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FiltroPacientes : ContentPage
+    {
+        public FiltroPacientes()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void searchPaciente_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
